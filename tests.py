786b6a12cc5d2435fa10b4a6bb7988504c9e4695
vsilent <jabberroid@gmail.com>
2019-06-05 15:09:54 +0300

    test service is running

--- tests.py
+++ tests.py
@@ -8,9 +8,7 @@ client = docker.from_env()
 
 # NGINX
 nginx = client.containers.get('nginx')
-nginx_cfg = nginx.exec_run("/usr/sbin/nginx -T")
 assert nginx.status == 'running'
-assert 'the configuration file /etc/nginx/nginx.conf syntax is ok' in nginx_cfg.output.decode()
 
 # test restart
 nginx.restart()
